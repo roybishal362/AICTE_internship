122497277373954a358aba212328a9f2631a6bc7
Bishal roy <144330743+roybishal362@users.noreply.github.com>
2024-12-25 18:11:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import spacy
+from spacy.cli import download
+
+# Download the 'en_core_web_sm' model if not already installed
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
